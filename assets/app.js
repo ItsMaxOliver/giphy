a2980adf57d1dd2da7c5c6865c886133c77f660c
MollyLJohnson <mollyl.johnson@att.net>
2018-03-06 17:22:36 -0600

    changed the linked jquery library, moved around the layout in html, still need to add a function to play the gifs when clicked

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,24 @@
 // Initial array of topics
-var topicArr = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+var topicArr = ["fail", "cry", "pain", "funny"];
 
 // Function for displaying buttons
 function renderButtons() {
 
-    // Delete the content inside the buttons-appear-here div prior to adding new movies, so that you don't get repeat buttons
+    // Delete the content inside the buttons-appear-here div prior to adding new topics, so that you don't get repeat buttons
     $("#buttons-appear-here").empty();
 
     // Loop through the array of topics, then generate buttons for each topic in the array
-    for ( let i = 0; i < topicArr.length; i++) {
+    for ( var i = 0; i < topicArr.length; i++) {
         var newButton = $("<button>" + topicArr[i] + "</button>");
         newButton.attr("data-term", topicArr[i]);
         $("#buttons-appear-here").append(newButton);
     }
-}
+};
 
-// This function handles events where the add movie button is clicked
+// This function handles events where the add button is clicked
 $("#add-button").on("click", function(event) {
     // event.preventDefault() prevents submit button from trying to send a form
     event.preventDefault();
-
     // grab the text the user types into the input field
     var input = $("#search-term").val().trim();
     // add the new topic into the topicArr
@@ -33,34 +32,33 @@ renderButtons();
 
 
 $("#buttons-appear-here").on("click", "button", function() {
+    // set term equal to the word/phrase that the user input
     var term = $(this).attr("data-term");
-    
+    // sets the queryURL to only get 10 gifs and only rated pg or lower
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + term + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
 
     $.get(queryURL).then(function(response) {
-
-        // since the image information is inside of the data key, make a variable named results and set it equal to response.data
         var results = response.data;
 
         for (var i = 0; i < results.length; i++) {
-        // Make a div with jQuery and store it in a variable named animalDiv.
-            var animalDiv = $("<div>");
-        // Make a paragraph tag with jQuery and store it in a variable named p.
+            // Made a div with jQuery and stored it in a variable named termDiv.
+            var termDiv = $("<div>");
+            // Made a paragraph tag with jQuery and stored it in a variable named p.
             var p = $("<p>");
-        // Set the inner text of the paragraph to the rating of the image in results[i].
+            // Set the inner text of the paragraph to the rating of the image in results[i].
             p.text("rating: " + results[i].rating);
-        // Make an image tag with jQuery and store it in a variable named animalImage.
-            var animalImage = $("<img>");
-        // Set the image's src to results[i]'s fixed_height.url.
-            animalImage.attr("src", results[i].images.fixed_height.url);
-        // Append the p variable to the animalDiv variable.
-            animalDiv.append(p);
-        // Append the animalImage variable to the animalDiv variable.
-            animalDiv.append(animalImage);
-        // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
-            $("#gifs-appear-here").prepend(animalDiv);
-
+            // Made an image tag with jQuery and stored it in a variable named termImage.
+            var termImage = $("<img>");
+            // Set the image's src to results[i]'s fixed_height_still.url so that it displays not playing when first loaded
+            termImage.attr("src", results[i].images.fixed_height_still.url);
+            // appends the term they clicked to each related gif displayed
+            termDiv.append("<h3>" + term + "</h3>");
+            // Append the termImage variable to the termDiv variable.
+            termDiv.append(termImage);
+            // Append the p variable to the termDiv variable.
+            termDiv.append(p);
+            // Prepend the termDiv variable to the element with an id of gifs-appear-here.
+            $("#gifs-appear-here").prepend(termDiv);
         }
-
         });
 });
